4af21ec90fa06243fde2eef1f8a576d2f6dc7583
zavashas <simazavadskis2808@gmail.com>
2024-02-13 15:01:20 +0300

    aaaaaaaaa

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const int rows = 3, columns = 5;
+    double numbers[rows][columns];
+
+    // Цикл для ввода
+    cout << "Введите элементы массива:" << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            cin >> numbers[i][j];
+        }
+    }
+    cout << endl;
+
+    // Цикл для вывода 
+    for (int i = 0; i < rows; i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < columns; j++)
+        {
+            cout << numbers[i][j] << "\t";
+            sum += numbers[i][j];
+        }
+        double mean = sum / columns;
+        cout << "Среднее: "  << mean << endl;
+    }
+
+    return 0;
+}
+
+
